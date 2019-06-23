d4c6c42c89273e1182b77e09cf5b3d53a3b0904b
DanNistor1 <dan.nistor1@yahoo.ro>
2019-06-23 14:06:02 +0300

    Add before test

--- src/test/java/com/company/ArrysUtilsTest.java
+++ src/test/java/com/company/ArrysUtilsTest.java
@@ -1,15 +1,23 @@
 package com.company;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArrysUtilsTest {
+
+    private ArraysUtils arraysUtils;
+
+    @Before
+    public void setUp(){
+        arraysUtils = new ArraysUtils();
+    }
 
     @Test
     public void binarySearchTest_elementExist(){
         int[] array = {1,3,5,7,9,10};
         int n=7;
-        int result = new ArraysUtils().binarySearch(array,n);
+        int result = arraysUtils.binarySearch(array,n);
         Assert.assertEquals(3,result);
     }
 
@@ -17,14 +25,14 @@ public class ArrysUtilsTest {
     public void binarySearchTest_elementNotExist(){
         int[] array = {1,3,5,7,9,10};
         int n=5;
-        int result = new ArraysUtils().binarySearch(array,n);
+        int result = arraysUtils.binarySearch(array,n);
         Assert.assertEquals(2,result);
     }
 
     @Test
     public void bubbleSort(){
         int[] array = {1,7,5,10,9};
-        new ArraysUtils().bubbleSort(array);
+        arraysUtils.bubbleSort(array);
         Assert.assertArrayEquals(new int[]{1,5,7,9,10}, array);
     }
 
